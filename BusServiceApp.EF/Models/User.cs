8f44aa95ae709805db33d8587f66e17ead8f19e1
Artem <samaliyadx@gmail.com>
2020-04-13 21:06:55 +0300

    Was created .core library project for entity framework

---
+++ BusServiceApp.EF/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusServiceApp.EF.Models
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+
+    }
+}
---
+++ BusServiceApp.EF/Models/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusServiceApp.EF.Models
+{
+    class UserContext : DbContext
+    {
+        private string _connectionString;
+
+        public DbSet<User> Users { get; set; }
+
+        public UserContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+    }
+}
